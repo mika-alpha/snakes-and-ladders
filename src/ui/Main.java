5533d4012b39a6a3f574cc64687d8599d5de0800
anima-anemone <78612129+anima-anemone@users.noreply.github.com>
2021-05-11 00:24:00 -0500

    Milestone1

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -2,6 +2,10 @@ package ui;
 
 public class Main {
 
+    /**
+     * made by: anemone
+     * github: anima-anemone
+     */
 
     public static void main(String[] args){
         Menu gameMenu = new Menu();
